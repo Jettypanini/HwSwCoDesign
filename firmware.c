26cc5095342cb402d2b69212dafdd2926b49dcb9
Jettypanini <jethro.pans@hotmail.com>
2023-02-20 10:15:47 +0100

    Week 02

--- firmware.c
+++ firmware.c
@@ -1,14 +1,20 @@
-#include "hamming.h"
 #include "print.h"
 
-void main(void)
-{
-	print_hex(6);
-	print_hex(get_hamming_weight(6));
-	print_hex(get_hamming_weight(47));
-	print_hex(get_hamming_distance(6,9));
-	print_hex(get_hamming_distance(35,98));
-	print_hex(get_factorial(6));
-	print_hex(get_factorial(11));
+extern unsigned int hwswcd_hd(unsigned int value1, unsigned int value2);
 
+void main(void) {
+    volatile unsigned int value1, value2, hd;
+
+    value1 = 5;
+    value2 = 9;
+
+    hd = hwswcd_hd(value1, value2);
+
+    print_str("HD(0x");
+    print_hex(value1, 2);
+    print_str(", ");
+    print_hex(value2, 2);
+    print_str(") = 0x");
+    print_hex(hd, 5);
+    print_str(".");
 }
