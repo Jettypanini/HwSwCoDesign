bf8d0c7e8dd72c575a74c0fcd51d8372df740d99
Jettypanini <jethro.pans@hotmail.com>
2023-03-12 16:04:02 +0100

    ex303

--- src/firmware.c
+++ src/firmware.c
@@ -1,15 +1,26 @@
 #include "print.h"
-#include "counter.h"
+#include "sine.h"
 
 void main(void) {
 
-	unsigned int counter_value;
+	unsigned int sine_value;
 	volatile unsigned char temp;
 
-	print_str("count: ");
-	counter_start();
-	for(temp=0;temp<10;temp++);
-	counter_stop();
-	counter_value = counter_get_value();
-	print_hex(counter_value, 2);
+	print_str("Sine value for 45 deg: ");
+	SINE_calc(45);
+	for(temp=0;temp<1;temp++);
+	sine_value = SINE_get_value();
+	print_hex(sine_value, 8);
+
+	print_str("Sine value for 210 deg: ");
+	SINE_calc(210);
+	for(temp=0;temp<1;temp++);
+	sine_value = SINE_get_value();
+	print_hex(sine_value, 8);
+
+	print_str("Sine value for 0 deg: ");
+	SINE_calc(0);
+	for(temp=0;temp<1;temp++);
+	sine_value = SINE_get_value();
+	print_hex(sine_value, 8);
 }
