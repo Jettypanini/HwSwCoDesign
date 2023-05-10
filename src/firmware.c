b199ef8473ae082fefd4f68b0ff9eae521272e62
Jettypanini <jethro.pans@hotmail.com>
2023-05-10 09:40:09 +0200

    fix code v9

--- src/firmware.c
+++ src/firmware.c
@@ -11,9 +11,9 @@ void main(void) {
     // digest for next message is: DBE69CB9DC29EB968ECF98EDC448A4D39B2207AEC83928CA5395B06B5EC9BBD1
     uint32_t message1[6] = {0x00010203, 0x04050607, 0x08090A0B, 0x0C0D0E0F, 0x10111213, 0x80000000};
 
-    uint8_t len2 = 8;
+    //uint8_t len2 = 8;
     // digest for next message is: DBE69CB9DC29EB968ECF98EDC448A4D39B2207AEC83928CA5395B06B5EC9BBD1
-    uint32_t message2[8] = {0x00010203, 0x04050607, 0x08090A0B, 0x0C0D0E0F, 0x10111213, 0x14151617, 0x18191A1B, 0x1C1D8000};
+    //uint32_t message2[8] = {0x00010203, 0x04050607, 0x08090A0B, 0x0C0D0E0F, 0x10111213, 0x14151617, 0x18191A1B, 0x1C1D8000};
 
     uint32_t digest[8];
 
@@ -25,10 +25,11 @@ void main(void) {
     }
     print_str(".");
 
-    ascon_hash(digest, message2, len2);
+    /*ascon_hash(digest, message2, len2);
 
     print_str("digest 1: 0x");
     for(int i=0; i<8; i++) {
         print_hex(digest[i], 8);
     }
-    print_str(".");
+    print_str(".");*/
+}
