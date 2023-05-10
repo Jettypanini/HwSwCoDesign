da7aa3c4ddb587bc8d9eb64019ee02dcf5b5ef62
Jettypanini <jethro.pans@hotmail.com>
2023-05-10 08:50:52 +0200

    fix code v8

--- src/ascon.c
+++ src/ascon.c
@@ -9,34 +9,26 @@ const uint8_t round_constants[12] = {0xf0, 0xe1, 0xd2, 0xc3, 0xb4, 0xa5, 0x96, 0
 const uint8_t sbox[32] = {0x04, 0x0b, 0x1f, 0x14, 0x1a, 0x15, 0x09, 0x02, 0x1b, 0x05, 0x08, 0x12, 0x1d, 0x03, 0x06, 0x1c, 0x1e, 0x13, 0x07, 0x0e, 0x00, 0x0d, 0x11, 0x18, 0x10, 0x0c, 0x01, 0x19, 0x16, 0x0a, 0x0f, 0x17};
 
 void ascon_hash(uint32_t * digest, uint32_t * message, uint8_t mlen) {
-    uint32_t message_padded[mlen];
-
-    for(int i = 0; i < (mlen); i++) {
-        message_padded[i] = message[i];
-    }
-
     //Initialization
     uint32_t internal_state[10] = {0x00400c00, 0x00000100, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    permutation(&internal_state, 12);
+    permutation(internal_state, 12);
 
     // Absorb message
-    for (int i = 0; i<(((mlen/2)-1)); i++) {
-        internal_state[0] = internal_state[0] ^ message_padded[i*2];
-        internal_state[1] = internal_state[1] ^ message_padded[(i*2)+1];
-        permutation(&internal_state, 12);
+
+
+    for (int i = 0; i<(((mlen/2))); i++) {
+        internal_state[0] = internal_state[0] ^ message[i*2];
+        internal_state[1] = internal_state[1] ^ message[(i*2)+1];
+        permutation(internal_state, 12);
     }
-    internal_state[0] = internal_state[0] ^ message_padded[mlen-2];
-    internal_state[1] = internal_state[1] ^ message_padded[mlen-1];
 
     //Squeeze hash
     for (int i = 0; i<4; i++) {
-        permutation(&internal_state, 12);
         digest[i*2] = internal_state[0];
         digest[(i*2)+1] = internal_state[1];
+        permutation(internal_state, 12);
     }
-
-    return;
 }
 
 void permutation(uint32_t * state, uint8_t number_of_rounds) {
@@ -47,26 +39,22 @@ void permutation(uint32_t * state, uint8_t number_of_rounds) {
     }
 
     for (int i=0; i<number_of_rounds; i++) {
-        pc(&temp_state, number_of_rounds, i);
-        ps(&temp_state);
-        pl(&temp_state);
+        pc(temp_state, number_of_rounds, i);
+        ps(temp_state);
+        pl(temp_state);
     }
 
     for (int i = 0; i<10; i++) {
         state[i] = temp_state[i];
     }
-
-    return;
 }
 
 // The function pc executes an xor with the round value from the round constants
 void pc(uint32_t * state, uint8_t round_number, uint8_t round) {
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 10; i++) {
         state[i] = state[i];
     }
-    state[9] = state[9] ^ round_constants[(12-round_number) + round];
-
-    return;
+    state[5] = state[5] ^ round_constants[(12-round_number) + round];
 }
 
 // For all 64 values, combined as X0 | X1 | X2 | X3 | X4 | X5, this function changes the value following the S-box
@@ -87,8 +75,6 @@ void ps(uint32_t * state) {
             state[(j*2)+1] |= ((temp2 >> (4-j)) & 1) << (31-i);
         }
     }
-
-    return;
 }
 
 // This function performs a few shift operations and XORs as given by the documentation
@@ -97,38 +83,36 @@ void pl(uint32_t * state) {
     uint32_t temp2[10];
 
     //X0
-    temp1[0] = state[0] >> 19;
-    temp1[1] = (state[1] >> 19) | (state[0] << (32-19));
-    temp2[0] = state[0] >> 28;
-    temp2[1] = (state[1] >> 28) | (state[0] << (32-28));
+    temp1[0] = (state[0] >> 19) ^ (state[1] << 13);
+    temp1[1] = (state[1] >> 19) ^ (state[0] << 13);
+    temp2[0] = (state[0] >> 28) ^ (state[1] << 4 );
+    temp2[1] = (state[1] >> 28) ^ (state[0] << 4 );
 
     //X1
-    temp1[2] = state[2] >> 61;
-    temp1[3] = (state[3] >> 61) | state[2] >> (61-32);
-    temp2[2] = state[2] >> 39;
-    temp2[3] = (state[3] >> 39) | state[3] >> (39-32);
+    temp1[2] = (state[2] << 3 ) ^ (state[3] >> 29);
+    temp1[3] = (state[3] << 3 ) ^ (state[2] >> 29);
+    temp2[2] = (state[2] << 25) ^ (state[3] >> 7 );
+    temp2[3] = (state[3] << 25) ^ (state[2] >> 7 );
 
     //X2
-    temp1[4] = state[4] >> 1;
-    temp1[5] = (state[5] >> 1) | state[4] << (32-1);
-    temp2[4] = state[4] >> 6;
-    temp2[5] = (state[5] >> 6) | state[4] << (32-6);
+    temp1[4] = (state[4] >> 1 ) ^ (state[5] << 31);
+    temp1[5] = (state[5] >> 1 ) ^ (state[4] << 31);
+    temp2[4] = (state[4] >> 6 ) ^ (state[5] << 26);
+    temp2[5] = (state[5] >> 6 ) ^ (state[4] << 26);
 
     //X3
-    temp1[6] = state[6] >> 10;
-    temp1[7] = (state[7] >> 10) | state[6] << (32-10);
-    temp2[6] = state[6] >> 17;
-    temp2[7] = (state[7] >> 17) | state[6] << (32-17);
+    temp1[6] = (state[6] >> 10) ^ (state[7] << 22);
+    temp1[7] = (state[7] >> 10) ^ (state[6] << 22);
+    temp2[6] = (state[6] >> 17) ^ (state[7] << 15);
+    temp2[7] = (state[7] >> 17) ^ (state[6] << 15);
 
     //X4
-    temp1[8] = state[8] >> 7;
-    temp1[9] = (state[9] >> 7) | state[8] << (32-7);
-    temp2[8] = state[8] >> 41;
-    temp2[9] = (state[9] >> 41) | state[8] >> (41-32);
+    temp1[8] = (state[8] >> 7 ) ^ (state[9] << 25);
+    temp1[9] = (state[9] >> 7 ) ^ (state[8] << 25);
+    temp2[8] = (state[8] << 23) ^ (state[9] >> 9 );
+    temp2[9] = (state[9] << 23) ^ (state[8] >> 9 );
 
     for(int i=0; i<10; i++) {
         state[i] = state[i] ^ temp1[i] ^ temp2[i];
     }
-
-    return;
 }
--- src/firmware.c
+++ src/firmware.c
@@ -7,7 +7,7 @@
 
 void main(void) {
 
-    /*uint8_t len1 = 6;
+    uint8_t len1 = 6;
     // digest for next message is: DBE69CB9DC29EB968ECF98EDC448A4D39B2207AEC83928CA5395B06B5EC9BBD1
     uint32_t message1[6] = {0x00010203, 0x04050607, 0x08090A0B, 0x0C0D0E0F, 0x10111213, 0x80000000};
 
@@ -17,7 +17,7 @@ void main(void) {
 
     uint32_t digest[8];
 
-    ascon_hash(&digest, &message1, len1);
+    ascon_hash(digest, message1, len1);
 
     print_str("digest 1: 0x");
     for(int i=0; i<8; i++) {
@@ -25,28 +25,10 @@ void main(void) {
     }
     print_str(".");
 
-    ascon_hash(&digest, &message2, len2);
-
-    print_str("digest 1: 0x");
-    for(int i=0; i<8; i++) {
-        print_hex(digest[i], 8);
-    }
-    print_str(".");*/
-
-    uint32_t digest[8] = {0x00400c00, 0x00000100, 0, 0, 0, 0, 0, 0};
+    ascon_hash(digest, message2, len2);
 
     print_str("digest 1: 0x");
     for(int i=0; i<8; i++) {
         print_hex(digest[i], 8);
     }
     print_str(".");
-
-}
-
-/*void memcpy(void *dest, const void *src, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        ((char*)dest)[i] = ((char*)src)[i];
-    }
-}*/
