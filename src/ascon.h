c4703bbca158821009680b81a799b1cf11dfa03f
Jettypanini <jethro.pans@hotmail.com>
2023-05-15 09:25:15 +0200

    load v2

--- src/ascon.h
+++ src/ascon.h
@@ -6,16 +6,18 @@
 #ifndef ASCON_ASCON_H
 #define ASCON_ASCON_H
 
-#define U32BIG(x)                                           \
+#define U32TOWORD(x)                                           \
   (((0x000000FF & (x)) << 24) | ((0x0000FF00 & (x)) << 8) | \
    ((0x00FF0000 & (x)) >> 8) | ((0xFF000000 & (x)) >> 24))
 
-uint32_t load(const uint32_t* inp, int len) {
-    uint32_t x;
-    for (int i=0; i<len; i++) {
-        x |= inp[í];
-    }
-  return x;
+uint32_t mask(int n) {
+  /* undefined for n == 0 */
+  return ~0ull >> (32 - 8 * n);
+}
+
+uint32_t load(const uint8_t* bytes, int n) {
+  uint64_t x = *(uint32_t*)bytes & mask(n);
+  return U32TOWORD(x);
 }
 
 void ascon_hash(uint32_t * digest, uint32_t * message, uint8_t mlen);
