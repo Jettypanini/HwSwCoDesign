2a05555c0a509852dd67f0858b78aa3881b03a49
Jettypanini <jethro.pans@hotmail.com>
2023-05-15 09:10:50 +0200

    load v1

--- src/ascon.h
+++ src/ascon.h
@@ -6,6 +6,18 @@
 #ifndef ASCON_ASCON_H
 #define ASCON_ASCON_H
 
+#define U32BIG(x)                                           \
+  (((0x000000FF & (x)) << 24) | ((0x0000FF00 & (x)) << 8) | \
+   ((0x00FF0000 & (x)) >> 8) | ((0xFF000000 & (x)) >> 24))
+
+uint32_t load(const uint32_t* inp, int len) {
+    uint32_t x;
+    for (int i=0; i<len; i++) {
+        x |= inp[í];
+    }
+  return x;
+}
+
 void ascon_hash(uint32_t * digest, uint32_t * message, uint8_t mlen);
 
 void permutation(uint32_t * state, uint8_t round_number);
